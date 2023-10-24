629e8dffc73450eabd80a58dbd96da4715780da1
luc14no12 <luciano.rodriguez.marcial@gmail.com>
2023-10-24 01:02:51 -0300

    finalizacion del punto 7

---
+++ js/js/tp2/7-.js
@@ -0,0 +1,13 @@
+let numero1 = parseInt(prompt("ingrese el primer numero"));
+let numero2 = parseInt(prompt("ingrese el segundo numero"));
+let numero3 = parseInt(prompt("ingrese el tercer numero"));
+
+let mayor = Math.max(numero1, numero2, numero3)
+
+console.log("el numero mas grande es el" + " " + mayor)
+
+
+
+
+
+
